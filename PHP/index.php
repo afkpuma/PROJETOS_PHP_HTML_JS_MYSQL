31b76cb5d263142a1ef1bb6a807ef4a2681165cc
Yam Alao <afk.puma@gmail.com>
2024-02-08 08:18:31 -0300

    .

---
+++ PHP/index.php
@@ -0,0 +1,54 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+
+<body>
+    <?php
+    
+
+    $usuario_possui_cartao_loja = true;
+    $valor_compra = 255;
+
+    $valor_frete = 50;
+    $recebeu_desconto_frete = false;
+
+    if ($usuario_possui_cartao_loja  AND $valor_compra >= 100) {
+        $valor_frete = 0;
+        $recebeu_desconto_frete = false;
+    }
+
+    ?>
+
+    <h1>Detalhes do pedido</h1>
+    <p>Possui cartão da loja?
+        <?php
+        if ($usuario_possui_cartao_loja) {
+            echo 'SIM';
+        } else {
+            echo 'NÃO';
+        }
+        ?>
+    </p>
+
+    <p>Valor da compra: <?= $valor_compra ?></p>
+
+    <p>Recebeu desconto no frete?
+        <?php
+        if ($recebeu_desconto_frete) {
+            echo 'SIM';
+        } else {
+            echo 'NÃO';
+        }
+        ?>
+    </p>
+
+    <p>Valor do frete: <?= $valor_frete ?></p>
+    ?> 
+</body>
+
+</html>
