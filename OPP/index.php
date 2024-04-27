866d4b8d48696e3f801cbf71af89f8fc433da410
Yam Alao <afk.puma@gmail.com>
2024-04-27 17:10:59 -0300

    dep_injection_mail

--- OPP/index.php
+++ OPP/index.php
@@ -7,25 +7,21 @@ class Funcionario
     public $nome = null;
     public $telefone = null;
     public $numFilhos = null;
+    public $cargo = null;
+    public $salario = null;
 
     //getter e setters
-    function __setNome($nome){
-         $this->nome  = $nome;
+    //overloading
+    function __set($atributo, $valor)
+    {
+        $this->$atributo = $valor;
     }
 
-    function __setNumFilhos($numFilhos){
-         $this->numFilhos  = $numFilhos;
+    function __get($atributo)
+    {
+        return $this->$atributo;
     }
 
-    function __getNome(){
-        return $this->nome;
-        
-    }
-    function __getNumFilhos(){
-        return $this->numFilhos;
-        
-    }
-    
 
     //metodos
     function resumirCardFunc()
@@ -40,14 +36,17 @@ class Funcionario
 }
 
 $y = new Funcionario();
-$y->__setNome('Jose');
-$y->__setNumFilhos(4);
-//echo $y->resumirCardFunc();
-echo $y->__getNome() . 'possui' . $y->__getNumFilhos() . 'filhos'; 
+$y->__set('nome', 'jose');
+$y->__set('numFilhos', 2);
+$y->__set('cargo', 'programador');
+$y->__set('salario', 'R$3.500');
 
-echo'<br />';
+//echo $y->resumirCardFunc();
+echo $y->__get('nome ') . 'possui' . $y->__get('numFilhos') . 'filhos';
+
+echo '<br />';
 
 $x = new Funcionario();
-$x->__setNome('Maria');
-$x->__setNumFilhos(0);
-echo $x->__getNome() . 'possui' . $x->__getNumFilhos() . 'filhos';
+$x->__set('nome', 'Maria');
+$x->__set('numFilhos', 0);
+echo $x->__get('nome') . 'possui' . $x->__get('numFilhos') . 'filhos';
